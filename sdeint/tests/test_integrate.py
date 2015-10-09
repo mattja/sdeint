066d11ecd5b4365fb784fa5c264b14bbf83685f3
Matthew Aburn <mattja6@gmail.com>
2015-10-09 23:53:54 +1000

    update tests

--- sdeint/tests/test_integrate.py
+++ sdeint/tests/test_integrate.py
@@ -9,7 +9,7 @@ s = np.random.randint(2**32)
 print('Testing using random seed %d' % s)
 np.random.seed(s)
 
-tspan = np.arange(0.0, 3000.0, 0.002)
+tspan = np.arange(0.0, 2000.0, 0.002)
 
 def test_mismatched_f():
     y0 = np.zeros(3)
@@ -37,19 +37,31 @@ def test_strat_1D_additive():
 def test_strat_ND_additive():
     y0 = np.zeros(3)
     def f(y, t):
-        return np.array([ -1.0*y[0], 
-                          y[2], 
+        return np.array([ -1.0*y[0],
+                          y[2],
                           -1.0*y[1] - 0.4*y[2] ])
-    def G(y, t): 
+    def G(y, t):
         return np.diag([0.2, 0.0, 0.5])
     y = sdeint.stratint(f, G, y0, tspan)
     w = np.fft.rfft(y[:, 2])
     # TODO assert spectral peak is around 1.0 radians/s
 
-def test_itoSRI2_1D_additive():
+def test_itoEuler_1D_additive():
     y0 = 0.0;
     f = lambda y, t: -1.0 * y
     G = lambda y, t: 0.2
-    y = sdeint.itoSRI2(f, G, y0, tspan)
+    y = sdeint.itoEuler(f, G, y0, tspan)
     assert(np.isclose(np.mean(y), 0.0, rtol=0, atol=1e-02))
     assert(np.isclose(np.var(y), 0.2*0.2/2, rtol=1e-01, atol=0))
+
+def test_stratHeun_ND_additive():
+    y0 = np.zeros(3)
+    def f(y, t):
+        return np.array([ -1.0*y[0],
+                          y[2],
+                          -1.0*y[1] - 0.4*y[2] ])
+    def G(y, t):
+        return np.diag([0.2, 0.0, 0.5])
+    y = sdeint.stratHeun(f, G, y0, tspan)
+    w = np.fft.rfft(y[:, 2])
+    # TODO assert spectral peak is around 1.0 radians/s
