1512e3486a8fd58c88202094ee740ed9632d1039
Matthew Aburn <mattja6@gmail.com>
2022-02-20 14:48:11 +1000

    start 0.2.5-dev

--- sdeint/__init__.py
+++ sdeint/__init__.py
@@ -4,4 +4,4 @@ from .wiener import deltaW, Ikpw, Jkpw, Iwik, Jwik
 from .integrate import (SDEValueError, itoint, stratint, itoEuler, stratHeun,
                         itoSRI2, stratSRS2, stratKP2iS)
 
-__version__ = '0.2.4'
+__version__ = '0.2.5-dev'
